82b2b71ccf8a33a0d9cdac29e2fc371af2b7e5c7
ni-shat <nishat.tabassum301@gmail.com>
2023-06-08 14:29:45 +0600

    basic structure is made

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('boss is running')
+})
+
+app.listen(port, () => {
+    console.log(`bistro boss is running on ${port}`);
+})
